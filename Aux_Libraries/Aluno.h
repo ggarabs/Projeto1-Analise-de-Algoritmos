a90d54af5faeb0e05ebf75a1960cbada20c87242
Anderson Nicodemo <125095374+Andernicod@users.noreply.github.com>
2023-09-17 18:37:47 -0300

    Comentarios

--- Aux_Libraries/Aluno.h
+++ Aux_Libraries/Aluno.h
@@ -1,16 +1,18 @@
+//Biblioteca que define a Struc Aluno
 #include <stdbool.h>
+//Define o tamanho maximo do vetor de caracteres como 150
 #define MAX_SIZE 150
 
-struct Aluno{
+struct Aluno{ //Estrutura Aluno
 	int semestre;
 	char turma, periodo, nome[MAX_SIZE], disciplina[MAX_SIZE];
 	double media;
 };
 
-typedef struct Aluno aluno;
+typedef struct Aluno aluno; //Coloca a estrutura aluno como typedef para otimizar o algortimo de busca
 
-bool isEqual(aluno a, aluno b){
-	bool ans = a.semestre == b.semestre;
+bool isEqual(aluno a, aluno b){ //Compara se aluno a é igual a aluno b
+	bool ans = a.semestre == b.semestre; //Usa ans para otimização de linhas no código
 	ans &= a.turma == b.turma;
 	ans &= a.periodo == b.periodo;
 	ans &= strcmp(a.nome, b.nome) == 0;
